614eb37b36dc459f5b1c0161905a5dc760e2e601
Raúl Caro Pastorino <raul@fryntiz.dev>
2022-12-29 11:33:13 +0100

    Initial Commit

---
+++ android/app/src/main/java/app/ionic/weather/station/MainActivity.java
@@ -0,0 +1,5 @@
+package app.ionic.weather.station;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.ionic.weather.station',
+  appName: 'app-ionic-weather-station',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
